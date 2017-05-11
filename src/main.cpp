591094ab3c394f38890a79d862dbceb574c14657
Cdfghglz <cdfghglz@gmail.com>
2017-05-11 22:50:24 +0200

    main refactoring, pendulum central star for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@ static int DISPLAY_HEIGHT;
 static const int ARM_LENGTH = 2;
 static const double INIT_THETA = 0.89;
 static const float BG_INIT_Z = -10.0f;
-//static const float REL_FACE_SCALE = 180.002f;
 
 int main(int argc, char **argv) {
 
@@ -28,13 +27,24 @@ int main(int argc, char **argv) {
 
 	Mesh star("../res/ee.obj");
 	Mesh background("../res/plane.obj");
-	Shader shader("../shd/basicShader");
+	Shader starShader("../shd/basicShader");
 	Shader bgShader("../shd/basicShader");
 	Texture texture("../res/bricks.jpg");
 
-	Transform transform;
-	Transform trs;
-	Camera camera(glm::vec3(0.0f, 0.0f, 20.0f), 70.0f, (float) DISPLAY_WIDTH / (float) DISPLAY_HEIGHT, 0.1f, 100.0f);
+#ifdef TRACK_DEBUG
+	Mesh centerStar("../res/ee.obj");
+	Shader centerStarShader("../shd/basicShader");
+	Transform centerStarTransform;
+	centerStarTransform.GetRot()->x = 1.57;        // Bring model to the initial orientation
+#endif
+
+	Transform starTransform;
+	Transform bgTransform;
+	Camera camera(glm::vec3(0.0f, 0.0f, 20.0f),
+				  70.0f,
+				  (float) DISPLAY_WIDTH / (float) DISPLAY_HEIGHT,
+				  0.1f,
+				  100.0f);
 
 	Pendulum<double> *pendulum = new Pendulum<double>(ARM_LENGTH, 10.0);
 	pendulum->reset(INIT_THETA);
@@ -42,11 +52,11 @@ int main(int argc, char **argv) {
 //	get background to the initial position and appropriate scale
 	Point<double> p = {1.0, 1.0};
 	camera.viewToWorld(&p, BG_INIT_Z);
-	trs.GetPos()->z = BG_INIT_Z;    // Bring bg plane to initial z distance
-	trs.GetScale()->x = (float) p.x;
-	trs.GetScale()->y = (float) p.x * DISPLAY_HEIGHT / DISPLAY_WIDTH;
+	bgTransform.GetPos()->z = BG_INIT_Z;    // Bring bg plane to initial z distance
+	bgTransform.GetScale()->x = (float) p.x;
+	bgTransform.GetScale()->y = (float) p.x * DISPLAY_HEIGHT / DISPLAY_WIDTH;
 
-	transform.GetRot()->x = 1.57;        // Bring model to the initial orientation
+	starTransform.GetRot()->x = 1.57;        // Bring model to the initial orientation
 
 //	other inits
 	display.captureCursor();
@@ -61,12 +71,12 @@ int main(int argc, char **argv) {
 			if (e.type == SDL_QUIT)
 				isRunning = false;
 //			else if (e.type == SDL_KEYDOWN) {
-//				transform.GetPos()->z += 0.5f;
-//				std::cout<< transform.GetPos()->z << std::endl;
+//				starTransform.GetPos()->z += 0.5f;
+//				std::cout<< starTransform.GetPos()->z << std::endl;
 //			}
 //			else if (e.type == SDL_KEYUP) {
-//				transform.GetPos()->z -= 0.5f;
-//				std::cout << transform.GetPos()->z << std::endl;
+//				starTransform.GetPos()->z -= 0.5f;
+//				std::cout << starTransform.GetPos()->z << std::endl;
 //			}
 			else if (e.type == SDL_KEYUP) {
 				Point<double> p = {0, 0};
@@ -94,7 +104,9 @@ int main(int argc, char **argv) {
 		ctr += 0.1f;
 		if (ctr <= 10) faceResult.valid = 1;
 		else if (ctr >= 20) ctr = 0.0f;
-		faceResult.facePos = {-centerPos.x, -centerPos.y, 8*sin(ctr)};
+
+		faceResult.facePos = {-centerPos.x, -centerPos.y, 0};
+//		faceResult.facePos = {-centerPos.x, -centerPos.y, 8*sin(ctr)};
 
 #else
 		cv::Point3d faceCenter = detector.detectFace();
@@ -104,25 +116,35 @@ int main(int argc, char **argv) {
 
 		faceResult.facePos = camera.viewToWorld(tracker.getPosition());
 		pendulum->setCenter(faceResult.facePos.x, faceResult.facePos.y, accel); // todo: implement the acceleration dependence on depth
-		transform.GetPos()->z = (float) faceCenter.z;
+		starTransform.GetPos()->z = (float) faceResult.facePos.z;
 
 		pendulum->step();
 		Point<double> pos = pendulum->getEndPosition();
 
-		transform.GetPos()->x = (float) pos.x;
-		transform.GetPos()->y = (float) pos.y;
+		starTransform.GetPos()->x = (float) pos.x;
+		starTransform.GetPos()->y = (float) pos.y;
 
 //		background
 		bgShader.Bind();
 		capTexture.Bind();
-		shader.Update(trs, camera);
+		starShader.Update(bgTransform, camera);
 		background.Draw();
 
 //		pendulum
-		shader.Bind();
+		starShader.Bind();
 		texture.Bind();
-		shader.Update(transform, camera);
+		starShader.Update(starTransform, camera);
 		star.Draw();
+
+#ifdef TRACK_DEBUG
+		centerStarTransform.GetPos()->x = (float) faceResult.facePos.x;
+		centerStarTransform.GetPos()->y = (float) faceResult.facePos.y;
+		centerStarTransform.GetPos()->z = (float) faceResult.facePos.z;
+		centerStarShader.Bind();
+		texture.Bind();
+		centerStarShader.Update(centerStarTransform, camera);
+		centerStar.Draw();
+#endif
 
 		display.SwapBuffers();
 		SDL_Delay(1);
